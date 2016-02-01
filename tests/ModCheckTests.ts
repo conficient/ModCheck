f8960b55e33644c654b975ec42981491908fb03b
Howard Richards <dev@conficient.com>
2016-02-01 09:55:57 +0000

    Added test .ts file (not compiling)

---
+++ tests/ModCheckTests.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../dist/typings/ModCheck.d.ts" />
+
+import modCheck = require("../artifacts/dev/ModCheck");
+
+describe("Basic function", function () {
+    it("ensure modCheck is working", function () {
+
+        expect(modCheck.AreValid(123456, 12345678)).toBe(false);
+    });
+});
+
