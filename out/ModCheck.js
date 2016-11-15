2f75e7d6a67e8fcdd464cbccff4af1249fbadbf1
Howard Richards <dev@conficient.com>
2016-11-15 14:44:20 +0000

    fixed package.json

---
+++ out/ModCheck.js
@@ -0,0 +1,12 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var ModCheck;
+    (function (ModCheck) {
+        function AreValid(sortCode, accountNumber) {
+            throw "not implemented";
+        }
+        ModCheck.AreValid = AreValid;
+    })(ModCheck || (ModCheck = {}));
+    return ModCheck;
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTW9kQ2hlY2suanMiLCJzb3VyY2VSb290IjoiYzovVXNlcnMvSG93YXJkL0RvY3VtZW50cy9HaXRIdWIvTW9kQ2hlY2svc3JjLyIsInNvdXJjZXMiOlsiTW9kQ2hlY2sudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7SUFDQSxJQUFPLFFBQVEsQ0FLZDtJQUxELFdBQU8sUUFBUTtRQUVYLGtCQUF5QixRQUF5QixFQUFFLGFBQThCO1lBQzlFLE1BQU0saUJBQWlCLENBQUM7UUFDNUIsQ0FBQztRQUZlLGlCQUFRLFdBRXZCLENBQUE7SUFDTCxDQUFDLEVBTE0sUUFBUSxLQUFSLFFBQVEsUUFLZDtJQUVELE9BQVMsUUFBUSxDQUFDIn0=
